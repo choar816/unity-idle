f8ddd54eec7b58de659261bd6b3ee3087ee63a58
Ahra Cho <choar816@postech.ac.kr>
2024-03-15 17:20:59 +0900

    Completed section 1 - ObjectManager

--- unity-idle/Assets/@Scripts/Controllers/Creature/Hero.cs
+++ unity-idle/Assets/@Scripts/Controllers/Creature/Hero.cs
@@ -5,6 +5,8 @@ using static Define;
 
 public class Hero : Creature
 {
+    Vector2 _moveDir = Vector2.zero;
+
     public override bool Init()
     {
         if (base.Init() == false)
@@ -14,6 +16,39 @@ public class Hero : Creature
         CreatureState = ECreatureState.Idle;
         Speed = 5.0f;
 
+        Managers.Game.OnMoveDirChanged -= HandleOnMoveDirChanged;
+        Managers.Game.OnMoveDirChanged += HandleOnMoveDirChanged;
+        Managers.Game.OnJoystickStateChanged -= HandleOnJoystickStateChanged;
+        Managers.Game.OnJoystickStateChanged += HandleOnJoystickStateChanged;
+
         return true;
+    }
+
+    void Update()
+    {
+        transform.Translate(_moveDir * Time.deltaTime * Speed);
+    }
+
+    private void HandleOnMoveDirChanged(Vector2 dir)
+    {
+        _moveDir = dir;
+        Debug.Log(dir);
+    }
+
+    private void HandleOnJoystickStateChanged(EJoystickState joystickState)
+    {
+        switch (joystickState)
+        {
+            case Define.EJoystickState.PointerDown:
+                CreatureState = Define.ECreatureState.Move;
+                break;
+            case Define.EJoystickState.Drag:
+                break;
+            case Define.EJoystickState.PointerUp:
+                CreatureState = Define.ECreatureState.Idle;
+                break;
+            default:
+                break;
+        }
     }
 }
