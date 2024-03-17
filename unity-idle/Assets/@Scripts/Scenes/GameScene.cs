44d6c2d2587a6001f58b074116dadbe17c60275a
Ahra Cho <choar816@postech.ac.kr>
2024-03-17 16:24:03 +0900

    Completed section 1 - Monster AI

--- unity-idle/Assets/@Scripts/Scenes/GameScene.cs
+++ unity-idle/Assets/@Scripts/Scenes/GameScene.cs
@@ -16,13 +16,18 @@ public class GameScene : BaseScene
         map.transform.position = Vector3.zero;
         map.name = "@BaseMap";
 
-        Hero hero = Managers.Object.Spawn<Hero>(Vector3.zero);
+        Hero hero = Managers.Object.Spawn<Hero>(new Vector3Int(-10, -5, 0));
         hero.CreatureState = ECreatureState.Move;
 
         CameraController camera = Camera.main.gameObject.GetOrAddComponent<CameraController>();
         camera.Target = hero;
 
         Managers.UI.ShowBaseUI<UI_Joystick>();
+
+        {
+            Monster monster = Managers.Object.Spawn<Monster>(new Vector3Int(0, 1, 0));
+            monster.CreatureState = ECreatureState.Idle;
+        }
 
         // TODO
 
