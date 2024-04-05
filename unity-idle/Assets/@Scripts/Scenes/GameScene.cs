74397dea959634eb050eb2cc3d224ac094a459e1
Ahra Cho <choar816@postech.ac.kr>
2024-04-05 16:07:13 +0900

    Add projectile motion code (section 1 - Projectile Motion)

--- unity-idle/Assets/@Scripts/Scenes/GameScene.cs
+++ unity-idle/Assets/@Scripts/Scenes/GameScene.cs
@@ -34,7 +34,8 @@ public class GameScene : BaseScene
         {
             Managers.Object.Spawn<Monster>(new Vector3Int(0, 1, 0), MONSTER_BEAR_ID);
             Managers.Object.Spawn<Monster>(new Vector3(1, 1, 0), MONSTER_SLIME_ID);
-            //Managers.Object.Spawn<Monster>(new Vector3(2, 1, 0), MONSTER_GOBLIN_ARCHER_ID);
+            Managers.Object.Spawn<Monster>(new Vector3(2, 1, 0), MONSTER_GOBLIN_ARCHER_ID);
+            Managers.Object.Spawn<Monster>(new Vector3(3, 1, 0), MONSTER_GOBLIN_ARCHER_ID);
         }
 
         {
