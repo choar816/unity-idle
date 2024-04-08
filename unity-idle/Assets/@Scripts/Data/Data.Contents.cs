20bb6b766dfa4dea28b445bac18a6da3d849565d
Ahra Cho <choar816@postech.ac.kr>
2024-04-08 22:58:24 +0900

    Edit data structure

--- unity-idle/Assets/@Scripts/Data/Data.Contents.cs
+++ unity-idle/Assets/@Scripts/Data/Data.Contents.cs
@@ -32,7 +32,6 @@ namespace Data
         public string SkeletonDataID;
         public string AnimatorName;
         public List<int> SkillIdList = new List<int>();
-        public int DropItemId;
     }
     #endregion
 
@@ -40,6 +39,7 @@ namespace Data
     [Serializable]
     public class MonsterData : CreatureData
     {
+        public int DropItemId;
     }
 
     [Serializable]
