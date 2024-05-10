f028fde8a22397b438fdcd7e4208778e866365ac
Ahra Cho <choar816@postech.ac.kr>
2024-05-10 15:53:41 +0900

    Integrate EObjectType and ECreatureType to EObjectType (section 3 - CreatureType 제거)

--- unity-idle/Assets/@Scripts/Managers/Contents/ObjectManager.cs
+++ unity-idle/Assets/@Scripts/Managers/Contents/ObjectManager.cs
@@ -60,24 +60,19 @@ public class ObjectManager
 
         BaseObject obj = go.GetComponent<BaseObject>();
 
-        if (obj.ObjectType == EObjectType.Creature)
+        if (obj.ObjectType == EObjectType.Hero)
         {
-            Creature creature = go.GetComponent<Creature>();
-            switch (creature.CreatureType)
-            {
-                case ECreatureType.Hero:
-                    obj.transform.parent = HeroRoot;
-                    Hero hero = creature as Hero;
-                    Heroes.Add(hero);
-                    break;
-                case ECreatureType.Monster:
-                    obj.transform.parent = MonsterRoot;
-                    Monster monster = creature as Monster;
-                    Monsters.Add(monster);
-                    break;
-            }
-
-            creature.SetInfo(templateID);
+            obj.transform.parent = HeroRoot;
+            Hero hero = go.GetComponent<Hero>();
+            Heroes.Add(hero);
+            hero.SetInfo(templateID);
+        }
+        else if (obj.ObjectType == EObjectType.Monster)
+        {
+            obj.transform.parent = HeroRoot;
+            Monster monster = go.GetComponent<Monster>();
+            Monsters.Add(monster);
+            monster.SetInfo(templateID);
         }
         else if (obj.ObjectType == EObjectType.Projectile)
         {
@@ -107,22 +102,15 @@ public class ObjectManager
 
     public void Despawn<T>(T obj) where T : BaseObject
     {
-        EObjectType objectType = obj.ObjectType;
-
-        if (obj.ObjectType == EObjectType.Creature)
+        if (obj.ObjectType == EObjectType.Hero)
         {
-            Creature creature = obj.GetComponent<Creature>();
-            switch (creature.CreatureType)
-            {
-                case ECreatureType.Hero:
-                    Hero hero = creature as Hero;
-                    Heroes.Remove(hero);
-                    break;
-                case ECreatureType.Monster:
-                    Monster monster = creature as Monster;
-                    Monsters.Remove(monster);
-                    break;
-            }
+            Hero hero = obj.GetComponent<Hero>();
+            Heroes.Remove(hero);
+        }
+        else if (obj.ObjectType == EObjectType.Monster)
+        {
+            Monster monster = obj.GetComponent<Monster>();
+            Monsters.Remove(monster);
         }
         else if (obj.ObjectType == EObjectType.Projectile)
         {
@@ -153,14 +141,14 @@ public class ObjectManager
         HashSet<Creature> targets = new HashSet<Creature>();
         HashSet<Creature> ret = new HashSet<Creature>();
 
-        ECreatureType targetType = Util.DetermineTargetType(owner.CreatureType, isAllies);
+		EObjectType targetType = Util.DetermineTargetType(owner.ObjectType, isAllies);
 
-        if (targetType == ECreatureType.Monster)
+        if (targetType == EObjectType.Monster)
         {
             var objs = Managers.Map.GatherObjects<Monster>(owner.transform.position, range, range);
             targets.AddRange(objs);
         }
-        else if (targetType == ECreatureType.Hero)
+        else if (targetType == EObjectType.Hero)
         {
             var objs = Managers.Map.GatherObjects<Hero>(owner.transform.position, range, range);
             targets.AddRange(objs);
@@ -199,14 +187,14 @@ public class ObjectManager
         HashSet<Creature> targets = new HashSet<Creature>();
         HashSet<Creature> ret = new HashSet<Creature>();
 
-        ECreatureType targetType = Util.DetermineTargetType(owner.CreatureType, isAllies);
+        EObjectType targetType = Util.DetermineTargetType(owner.ObjectType, isAllies);
 
-        if (targetType == ECreatureType.Monster)
+        if (targetType == EObjectType.Monster)
         {
             var objs = Managers.Map.GatherObjects<Monster>(owner.transform.position, range, range);
             targets.AddRange(objs);
         }
-        else if (targetType == ECreatureType.Hero)
+        else if (targetType == EObjectType.Hero)
         {
             var objs = Managers.Map.GatherObjects<Hero>(owner.transform.position, range, range);
             targets.AddRange(objs);
