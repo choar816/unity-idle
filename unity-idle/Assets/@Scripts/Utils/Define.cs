980b71cf934c5a06385f5659d10ef11bae9a9781
Ahra Cho <choar816@postech.ac.kr>
2024-06-03 21:37:04 +0900

    Add item data (section 3 - ItemData)

--- unity-idle/Assets/@Scripts/Utils/Define.cs
+++ unity-idle/Assets/@Scripts/Utils/Define.cs
@@ -203,6 +203,52 @@ public static class Define
         Japanese
     }
 
+    public enum EItemGrade
+    {
+        None,
+        Normal,
+        Rare,
+        Epic,
+        Legendary
+    }
+
+    public enum EItemGroupType
+    {
+        None,
+        Equipment,
+        Consumable,
+    }
+
+    public enum EItemType
+    {
+        None,
+        Weapon,
+        Armor,
+        Potion,
+        Scroll
+    }
+
+    public enum EItemSubType
+    {
+        None,
+
+        Sword,
+        Dagger,
+        Bow,
+
+        Helmet,
+        Armor,
+        Shield,
+        Gloves,
+        Shoes,
+
+        EnchantWeapon,
+        EnchantArmor,
+
+        HealthPotion,
+        ManaPotion,
+    }
+
 
     public const float EFFECT_SMALL_RADIUS = 2.5f;
     public const float EFFECT_NORMAL_RADIUS = 4.5f;
