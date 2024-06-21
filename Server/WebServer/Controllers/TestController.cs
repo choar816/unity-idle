82d14ed13ef9c9c6e1e989c953bffaefc0f58c35
Ahra Cho <choar816@postech.ac.kr>
2024-06-21 22:12:30 +0900

    Add web server (section 4 - ASP.NET WebAPI)

---
+++ Server/WebServer/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebServer.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class TestController : ControllerBase
+    {
+        [HttpPost]
+        [Route("hello")]
+        public TestPacketRes TestPost([FromBody] TestPacketReq value)
+        {
+            TestPacketRes result = new TestPacketRes();
+            result.success = true;
+
+            return result;
+        }
+    }
+}
---
+++ Server/WebServer/Data/WebPacket.cs
@@ -0,0 +1,12 @@
+﻿[Serializable]
+public class TestPacketReq
+{
+    public string userId { get; set; }
+    public string token { get; set; }
+}
+
+[Serializable]
+public class TestPacketRes
+{
+    public bool success { get; set; }
+}
